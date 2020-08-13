6475877ddd7a8d29447885acf55b7320e68ffc37
nerdycheetah <69652800+nerdycheetah@users.noreply.github.com>
2020-08-13 14:24:14 -0700

    find max profit from buying selling stocks

---
+++ stock-price.py
@@ -0,0 +1,8 @@
+'''
+Given a list of stock prices throughout a week, return the highest profit that could be made for that week. This assumes buying and selling stocks work in chronological order, in the same week you cannot buy a stock on Friday and sell it on Wednesday.
+
+[100, 200, 50, 250, 700]
+'''
+
+min_price
+max
